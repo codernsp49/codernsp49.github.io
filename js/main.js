ed4025834e76eabfa2a2e304a69746e3acf8aee2
codernsp49 <codernsp49@gmail.com>
2026-01-31 10:55:48 +0530

    Implement typing effect for roles in main.js
    Added typing and erasing effects for role display.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-// JavaScript will be added step by step
+const roles = [
+  "BCA Student",
+  "Developer",
+  "Tool Builder"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+let typingElement = document.getElementById("typing");
+
+function typeEffect() {
+  if (charIndex < roles[roleIndex].length) {
+    typingElement.textContent += roles[roleIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeEffect, 100);
+  } else {
+    setTimeout(eraseEffect, 1500);
+  }
+}
+
+function eraseEffect() {
+  if (charIndex > 0) {
+    typingElement.textContent =
+      roles[roleIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseEffect, 60);
+  } else {
+    roleIndex = (roleIndex + 1) % roles.length;
+    setTimeout(typeEffect, 300);
+  }
+}
+
+typeEffect();// JavaScript will be added step by step
